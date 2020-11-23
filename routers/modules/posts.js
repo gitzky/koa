172e5aeec465c5646b5581187d4f8c1155f7cae1
gitzky <927926261@qq.com>
2020-11-23 15:02:14 +0800

    update

--- routers/modules/posts.js
+++ routers/modules/posts.js
@@ -14,4 +14,41 @@ router.post('/selPostList', async (ctx, next) => {
     })
 })
 
+router.post('/selPostById', async (ctx, next) => {
+  var id = ctx.request.body.id || null
+  console.log('id--------', id)
+  await db('posts')
+    .where({id})
+    .find()
+    .then((data) => {
+      var json = { code: '0', data, msg: 'success' }
+      ctx.body = JSON.stringify(json)
+    })
+})
+
+router.post('/addPost', async (ctx, next) => {
+  console.log('ctx.state--------',     ctx.state,)
+  var datas = {
+    uid: ctx.state.data._id,
+    name: ctx.request.body.name,
+    title: ctx.request.body.title,
+    content: ctx.request.body.content,
+    md: ctx.request.body.md,
+    moment: ctx.request.body.moment,
+    coomments: ctx.request.body.coomments,
+    pv: ctx.request.body.pv,
+    avator: ctx.request.body.avator
+  }
+  
+  console.log('uid--------', datas)
+  
+  await db('posts')
+    .add(datas)
+    .then((data) => {
+      var json = { code: '0', data, msg: 'success' }
+      ctx.body = JSON.stringify(json)
+    })
+})
+
+
 module.exports = router.routes()
